42b6d05918c07c9b625793b125d2c9a7d5233600
ataSky <47087713+atasky@users.noreply.github.com>
2024-07-20 15:54:04 +0300

    Update vicidial-install-c8.sh

--- vicidial-install-c8.sh
+++ vicidial-install-c8.sh
@@ -7,17 +7,17 @@ export LC_ALL=C
 
 yum groupinstall "Development Tools" -y
 
-yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-yum -y install http://rpms.remirepo.net/enterprise/remi-release-8.rpm
-yum -y install yum-utils
+yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm --skip-broken
+yum -y install http://rpms.remirepo.net/enterprise/remi-release-8.rpm --skip-broken
+yum -y install yum-utils --skip-broken
 #dnf module enable php:remi-5.6 -y
 dnf module enable mariadb:10.5 -y
 
-dnf -y install dnf-plugins-core
+dnf -y install dnf-plugins-core --skip-broken
 dnf config-manager --set-enabled powertools
 
 yum install -y 'php56 php56-php-mcrypt php56-php-cli php56-php-gd php56-php-curl php56-php-mysql php56-php-ldap php56-php-zip php56-php-fileinfo php56-php-opcache wget unzip make patch gcc gcc-c++ subversion php56-php-devel gd-devel readline-devel php56-php-mbstring php56-php-imap php56-php-odbc php56-php-pear php56-php-xml php56-php-xmlrpc curl curl-devel perl-libwww-perl ImageMagick libxml2 libxml2-devel httpd libpcap libpcap-devel libnet ncurses ncurses-devel screen kernel* mutt glibc.i686 certbot python3-certbot-apache mod_ssl openssl-devel newt-devel kernel-devel sqlite-devel libuuid-devel sox sendmail lame-devel htop iftop perl-File-Which libss7 mariadb-devel libss7* libopen*' --skip-broken
-yum -y install sqlite-devel
+yum -y install sqlite-devel --skip-broken
 
 
 tee -a /etc/httpd/conf/httpd.conf <<EOF
@@ -51,7 +51,7 @@ EOF
 systemctl restart httpd
 
 
-dnf install -y mariadb-server mariadb
+dnf install -y mariadb-server mariadb --skip-broken
 
 
 systemctl enable mariadb
@@ -149,18 +149,18 @@ systemctl restart mariadb.service
 
 echo "Install Perl"
 
-yum install -y perl-CPAN perl-YAML perl-CPAN-DistnameInfo perl-libwww-perl perl-DBI perl-DBD-MySQL perl-GD perl-Env perl-Term-ReadLine-Gnu perl-SelfLoader perl-open.noarch 
+yum install -y perl-CPAN perl-YAML perl-CPAN-DistnameInfo perl-libwww-perl perl-DBI perl-DBD-MySQL perl-GD perl-Env perl-Term-ReadLine-Gnu perl-SelfLoader perl-open.noarch --skip-broken
 
 cpan -i Tk String::CRC Tk::TableMatrix Net::Address::IP::Local Term::ReadLine::Gnu XML::Twig Digest::Perl::MD5 Spreadsheet::Read Net::Address::IPv4::Local RPM::Specfile Spreadsheet::XLSX Spreadsheet::ReadSXC MD5 Digest::MD5 Digest::SHA1 Bundle::CPAN Pod::Usage Getopt::Long DBI DBD::mysql Net::Telnet Time::HiRes Net::Server Mail::Sendmail Unicode::Map Jcode Spreadsheet::WriteExcel OLE::Storage_Lite Proc::ProcessTable IO::Scalar Scalar::Util Spreadsheet::ParseExcel Archive::Zip Compress::Raw::Zlib Spreadsheet::XLSX Test::Tester Spreadsheet::ReadSXC Text::CSV Test::NoWarnings Text::CSV_PP File::Temp Text::CSV_XS Spreadsheet::Read LWP::UserAgent HTML::Entities HTML::Strip HTML::FormatText HTML::TreeBuilder Switch Time::Local Mail::POP3Client Mail::IMAPClient Mail::Message IO::Socket::SSL readline
 
 echo "Please Press ENTER for CPAN Install"
 
-yum install perl-CPAN -y
-yum install perl-YAML -y
-yum install perl-libwww-perl -y
-yum install perl-DBI -y
-yum install perl-DBD-MySQL -y
-yum install perl-GD -y
+yum install perl-CPAN -y --skip-broken
+yum install perl-YAML -y --skip-broken
+yum install perl-libwww-perl -y --skip-broken
+yum install perl-DBI -y --skip-broken
+yum install perl-DBD-MySQL -y --skip-broken
+yum install perl-GD -y --skip-broken
 cd /usr/bin/
 curl -LOk http://xrl.us/cpanm
 chmod +x cpanm
@@ -330,7 +330,7 @@ make
 make install
 make install-config
 
-yum -y install dahdi-tools-libs
+yum -y install dahdi-tools-libs --skip-broken
 
 cd tools
 make clean
@@ -674,7 +674,7 @@ systemctl enable rc-local
 systemctl start rc-local
 
 ##Install Dynportal
-yum install -y firewalld
+yum install -y firewalld --skip-broken
 cd /home
 wget https://dialer.one/dynportal.zip
 wget https://dialer.one/firewall.zip
