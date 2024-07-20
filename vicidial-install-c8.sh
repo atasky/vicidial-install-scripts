c43341afd900feb7f1b469c03c4c2501997f3a9f
ataSky <47087713+atasky@users.noreply.github.com>
2024-07-20 16:22:43 +0300

    Update vicidial-install-c8.sh

--- vicidial-install-c8.sh
+++ vicidial-install-c8.sh
@@ -16,8 +16,9 @@ dnf module enable mariadb:10.5 -y
 dnf -y install dnf-plugins-core --skip-broken
 dnf config-manager --set-enabled powertools
 
+yum -y install gcc gcc-c++ --allowerasing
 yum -y install httpd httpd-devel httpd-tools
-yum install -y php56 php56-php-mcrypt php56-php-cli php56-php-gd php56-php-curl php56-php-mysql php56-php-ldap php56-php-zip php56-php-fileinfo php56-php-opcache wget unzip make patch gcc gcc-c++ subversion php56-php-devel gd-devel readline-devel php56-php-mbstring php56-php-imap php56-php-odbc php56-php-pear php56-php-xml php56-php-xmlrpc curl curl-devel perl-libwww-perl ImageMagick libxml2 libxml2-devel libpcap libpcap-devel libnet ncurses ncurses-devel screen kernel* mutt glibc.i686 certbot python3-certbot-apache mod_ssl openssl-devel newt-devel kernel-devel sqlite-devel libuuid-devel sox sendmail lame-devel htop iftop perl-File-Which libss7 mariadb-devel libss7* libopen*
+yum install -y php56 php56-php-mcrypt php56-php-cli php56-php-gd php56-php-curl php56-php-mysql php56-php-ldap php56-php-zip php56-php-fileinfo php56-php-opcache wget unzip make patch subversion php56-php-devel gd-devel readline-devel php56-php-mbstring php56-php-imap php56-php-odbc php56-php-pear php56-php-xml php56-php-xmlrpc curl curl-devel perl-libwww-perl ImageMagick libxml2 libxml2-devel libpcap libpcap-devel libnet ncurses ncurses-devel screen kernel* mutt glibc.i686 certbot python3-certbot-apache mod_ssl openssl-devel newt-devel kernel-devel sqlite-devel libuuid-devel sox sendmail lame-devel htop iftop perl-File-Which libss7 mariadb-devel libss7* libopen*
 yum -y install sqlite-devel --skip-broken
 
 
@@ -764,6 +765,18 @@ sox -t ul -r 8000 -c 1 ../mohmp3/manolo_camp-morning_coffee.ulaw -t ul manolo_ca
 
 chmod 777 /var/spool/asterisk/monitorDONE
 chkconfig asterisk off
+
+# mtop install
+cd /usr/local/src
+wget http://download.vicidial.com/required-apps/mtop-0.6.6.tar.gz
+tar -zxvf mtop-0.6.6.tar.gz
+cd mtop-*
+perl Makefile.PL
+make
+make install
+
+
+
 
 read -p 'Press Enter to Reboot: '
 
