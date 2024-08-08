2539ff6e994ae7b407c189da7921140b1d2c5cfc
ataSky <47087713+atasky@users.noreply.github.com>
2024-08-08 04:49:05 +0300

    Update vicidial-install-c7.sh

--- vicidial-install-c7.sh
+++ vicidial-install-c7.sh
@@ -41,7 +41,13 @@ XMARIADBX
 yum -y groupinstall "Development Tools"
 yum -y install gcc gcc-c++ 
 yum -y install httpd httpd-devel httpd-tools 
-yum -y install php56 php56-syspaths php56-php-mcrypt php56-php-cli php56-php-gd php56-php-curl php56-php-mysql php56-php-ldap php56-php-zip php56-php-fileinfo php56-php-opcache wget unzip make patch subversion php56-php-devel gd-devel readline-devel php56-php-mbstring php56-php-imap php56-php-odbc php56-php-pear php56-php-xml php56-php-xmlrpc curl curl-devel perl-libwww-perl ImageMagick libxml2 libxml2-devel libpcap libpcap-devel libnet ncurses ncurses-devel screen mutt glibc certbot python3-certbot-apache mod_ssl openssl-devel newt-devel sqlite-devel libuuid-devel sox sendmail lame-devel htop iftop perl-File-Which libss7 wget mariadb-devel libss7* libopen* libxml2.i686 glibc.i686 speex-devel libcurl-devel
+yum -y install libuuid-devel
+yum -y install ncurses-devel
+yum -y install libxml2-devel
+yum -y install sqlite-devel
+yum -y install jansson-devel
+
+yum -y install "php56 php56-syspaths php56-php-mcrypt php56-php-cli php56-php-gd php56-php-curl php56-php-mysql php56-php-ldap php56-php-zip php56-php-fileinfo php56-php-opcache wget unzip make patch subversion php56-php-devel gd-devel readline-devel php56-php-mbstring php56-php-imap php56-php-odbc php56-php-pear php56-php-xml php56-php-xmlrpc curl curl-devel perl-libwww-perl ImageMagick libxml2 libxml2-devel libpcap libpcap-devel libnet ncurses ncurses-devel screen mutt glibc certbot python3-certbot-apache mod_ssl openssl-devel newt-devel sqlite-devel libuuid-devel sox sendmail lame-devel htop iftop perl-File-Which libss7 wget mariadb-devel libss7* libopen* libxml2.i686 glibc.i686 speex-devel libcurl-devel"
 ln -s /lib64/libtinfo.so.5 /lib64/libtermcap.so.2
 chmod u+x /opt/vicidial-install-scripts/install_prereq
 /opt/vicidial-install-scripts/install_prereq install
@@ -180,7 +186,7 @@ yum install perl-libwww-perl -y
 yum install perl-DBI -y
 yum install perl-DBD-MySQL -y
 yum install perl-GD -y
-
+yum install perl-core -y
 echo "Please Press ENTER for CPAN Install"
 
 cd /usr/bin/
