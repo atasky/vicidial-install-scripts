6ad3263f17ef476ab8b28737a69075e81b191c0d
ataSky <47087713+atasky@users.noreply.github.com>
2024-08-08 04:21:22 +0300

    Update vicidial-install-c7.sh

--- vicidial-install-c7.sh
+++ vicidial-install-c7.sh
@@ -22,7 +22,6 @@ rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7
 rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-raven
 
 #echo "exclude=*i686*" >> /etc/yum.conf
-ln -s /lib64/libtinfo.so.5 /lib64/libtermcap.so.2
 
 # yum plugins
 cat <<yumdisableplugins>> /etc/yum/pluginconf.d/search-disabled-repos.conf
@@ -43,6 +42,7 @@ yum -y groupinstall "Development Tools"
 yum -y install gcc gcc-c++ 
 yum -y install httpd httpd-devel httpd-tools 
 yum -y install php56 php56-syspaths php56-php-mcrypt php56-php-cli php56-php-gd php56-php-curl php56-php-mysql php56-php-ldap php56-php-zip php56-php-fileinfo php56-php-opcache wget unzip make patch subversion php56-php-devel gd-devel readline-devel php56-php-mbstring php56-php-imap php56-php-odbc php56-php-pear php56-php-xml php56-php-xmlrpc curl curl-devel perl-libwww-perl ImageMagick libxml2 libxml2-devel libpcap libpcap-devel libnet ncurses ncurses-devel screen mutt glibc certbot python3-certbot-apache mod_ssl openssl-devel newt-devel sqlite-devel libuuid-devel sox sendmail lame-devel htop iftop perl-File-Which libss7 wget mariadb-devel libss7* libopen* libxml2.i686 glibc.i686
+ln -s /lib64/libtinfo.so.5 /lib64/libtermcap.so.2
 
 tee -a /etc/httpd/conf/httpd.conf <<EOF
 
